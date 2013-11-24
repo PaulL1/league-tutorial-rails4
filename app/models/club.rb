fb9357e568f596867fec434515202a0ead3f8a97
Paul Lambert <paul@planar.id.au>
2013-11-24 16:37:39 +1100

    More validation in clubs controller

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,3 +1,5 @@
 class Club < ActiveRecord::Base
-  validates_presence_of :name
+  validates_presence_of :name, :contact_officer
+  validates_length_of :name, :in => 5..30
+  validates_length_of :contact_officer, :in => 9..25
 end
